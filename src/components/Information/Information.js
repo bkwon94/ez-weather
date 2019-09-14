c827657e462461c8998e62a315568e111ea516b7
Brian Kwon <bkwon94@gmail.com>
2019-09-13 23:11:34 -0400

    Add conditional render to info component and access to logo

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -2,37 +2,44 @@ import React from 'react';
 import './Information.css';
 
 const Information = ({ current }) => {
-  return (
-    <div>
+  if (current) {
+    return (
       <div>
-        {current.weather_state_name}
+        <div><img src={`https://www.metaweather.com/static/img/weather/${current.weather_state_abbr}.svg`} alt="logo"></img></div>
+        <div>
+          {current.weather_state_name}
+        </div>
+        <div>
+          {current.the_temp}
+        </div>
+        <div>
+          {current.min_temp}
+        </div>
+        <div>
+          {current.max_temp}
+        </div>
+        <div>
+          {current.wind_speed}
+        </div>
+        <div>
+          {current.wind_direction}
+        </div>
+        <div>
+          {current.air_pressure}
+        </div>
+        <div>
+          {current.humidity}
+        </div>
+        <div>
+          {current.visibility}
+        </div>
       </div>
-      <div>
-        {current.the_temp}
-      </div>
-      <div>
-        {current.min_temp}
-      </div>
-      <div>
-        {current.max_temp}
-      </div>
-      <div>
-        {current.wind_speed}
-      </div>
-      <div>
-        {current.wind_direction}
-      </div>
-      <div>
-        {current.air_pressure}
-      </div>
-      <div>
-        {current.humidity}
-      </div>
-      <div>
-        {current.visibility}
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div></div>
+    )
+  }
 }
 
 export default Information;
