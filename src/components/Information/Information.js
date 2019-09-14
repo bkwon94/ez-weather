70253fb98ae9bdfc16a699203a3dbbf77ff8bbd1
Brian Kwon <bkwon94@gmail.com>
2019-09-14 00:32:28 -0400

    Begin basic styling

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -6,25 +6,20 @@ const Information = ({ current, city }) => {
     return (
       <div className="info-container">
         <h1>{city}</h1>
-        <div>
-          {Math.round(current.the_temp)}&#8451;
+        <div className="current-degree">
+          {Math.round(current.the_temp)}&deg;
         </div>
+        <img src={`https://www.metaweather.com/static/img/weather/${current.weather_state_abbr}.svg`} alt="logo"></img>
 
         <div>
           {current.weather_state_name}
-          <img src={`https://www.metaweather.com/static/img/weather/${current.weather_state_abbr}.svg`} alt="logo"></img>
         </div>
         <div>
-          High: {Math.round(current.max_temp)}
+          {Math.round(current.max_temp)} / {Math.round(current.min_temp)}
         </div>
+
         <div>
-          Low: {Math.round(current.min_temp)}
-        </div>
-        <div>
-          Wind Speed: {Math.round(current.wind_speed)} mph
-        </div>
-        <div>
-          Wind Direction: {current.wind_direction_compass}
+          Wind: {current.wind_direction_compass} {Math.round(current.wind_speed)} MPH
         </div>
         <div>
           Air Pressure: {Math.round(current.air_pressure)}
