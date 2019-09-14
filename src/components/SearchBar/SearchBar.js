f88da1c35d0152a1fc6d2fd9f17e24870611356c
Brian Kwon <bkwon94@gmail.com>
2019-09-13 20:43:07 -0400

    Add searchQuery in state to keep track of user input

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 import './SearchBar.css';
 
 const SearchBar = () => {
+
+  const [searchQuery, setSearchQuery] = useState('');
+
   return (
     <div>
-      <input type="text" name="search" placeholder="Search"/>
+      <input type="text" name="search" placeholder="Search" onChange={e => setSearchQuery(e.target.value)}/>
       <button>Search</button>
     </div>
   );
